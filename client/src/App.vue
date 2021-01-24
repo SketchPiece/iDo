f26044395b86850740c971c8f807eb24beff406f
SketchPiece <lubkin.andrey@gmail.com>
2021-01-24 16:37:13 +0200

    mobile warning was added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <Header />
+  <MobileBanner v-if="winWidth < 800" />
   <router-view v-slot="{ Component }">
     <transition name="fade" mode="out-in">
       <component :is="Component" />
@@ -9,12 +10,32 @@
 
 <script>
 import Header from './components/Header'
+import MobileBanner from './components/MobileBanner'
 import './index.scss'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    MobileBanner
+  },
+  data: () => ({
+    winWidth: window.innerWidth
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.onResize)
+    })
+  },
+
+  beforeUnmount() {
+    window.removeEventListener('resize', this.onResize)
+  },
+
+  methods: {
+    onResize() {
+      this.winWidth = window.innerWidth
+    }
   }
 }
 </script>
