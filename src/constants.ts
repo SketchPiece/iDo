9a8313eb3a04d561487f5abdfb0e68aab0c3b100
SketchPiece <lubkin.andrey@gmail.com>
2021-04-01 21:55:16 +0300

    backend config refactor

--- src/constants.ts
+++ src/constants.ts
@@ -1,21 +1,22 @@
 export const JWT_SECRET = 'supersecretsecretcode'
 export const JWT_EXPIRES_IN = 60 * 60 * 24
 
-export const DB_OPTIONS =
-  process.env.NODE_ENV === 'dev'
-    ? {
-        type: 'sqlite',
-        database: 'db',
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true
-      }
-    : {
-        type: 'postgres',
-        host: process.env.DATABASE_URL,
-        port: process.env.DATABASE_PORT,
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        entities: [__dirname + '/../**/*.entity.ts'],
-        synchronize: true
-      }
+const DEV_DB = {
+  type: 'sqlite',
+  database: 'db',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true
+}
+
+const PROD_DB = {
+  type: 'postgres',
+  host: process.env.DATABASE_URL,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true
+}
+
+export const DB_OPTIONS = process.env.NODE_ENV === 'dev' ? DEV_DB : PROD_DB
